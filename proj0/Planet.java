fb99ffd4114c2d9cc00cef170f5ba25e8ed2d403
ziqi <ziqi>
2018-12-06 10:43:21 -0500

    add planet

---
+++ proj0/Planet.java
@@ -0,0 +1,15 @@
+public class Planet{
+	double xxPos;
+	double yyPos;
+	double xxVel;
+	double yyVel;
+	double mass;
+	String imgFileNmae;
+	public Planet(double xP, double yP, double xV, double yV, double m, String img){
+
+	}
+	public Planet(Planet p){
+		
+	}
+
+}
