1ecdb163b94fb998021d42b5c4051d4ad2fbf2f8
ziqili123 <ziqil@umich.edu>
2019-01-27 16:40:34 -0500

    Jan22Proj1A

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,12 +1,12 @@
-public class ArrayDeque<Item> {
+public class ArrayDeque<T> {
     private int size;
     private int first;
     private int last;
     private int step;
-    private Item newList [];
+    private T newList [];
     public ArrayDeque (){
 
-        newList = (Item[]) new Object[8];
+        newList = (T[]) new Object[8];
         size = 0;
         first = 7;
         last = 0;
@@ -18,17 +18,17 @@ public class ArrayDeque<Item> {
 
     }*/
    public void resize(int size){
-       Item firstlist[];
-       Item b[];
-       firstlist = (Item[]) new Object[size + 1];
-       b = (Item[]) new Object[size - last];
+       T firstlist[];
+      T b[];
+       firstlist = (T[]) new Object[size + 1];
+       b = (T[]) new Object[size - last];
        System.arraycopy(newList,last,b,0,size - last);
        System.arraycopy(newList,0,firstlist,0,last);
        System.arraycopy(b,0,firstlist,first + 2, size - last);
        newList = firstlist;
 
    }
-    public void addLast(Item x){
+    public void addLast(T x){
        if(step > 7 ){
            resize(size);
            newList[last] = x;
@@ -43,7 +43,7 @@ public class ArrayDeque<Item> {
        size += 1;
        step += 1;
     }
-    public void addFirst(Item x){
+    public void addFirst(T x){
         if(step > 7){
            resize(size);
            newList[last] = x;
@@ -56,32 +56,40 @@ public class ArrayDeque<Item> {
         size += 1;
         step += 1;
     }
-    public Item removeLast(){
-        Item c;
-        c = newList[last-1];
-        newList[last - 1] = null;
-        last -= 1 ;
-        size -= 1;
-        step -= 1;
-        return c;
-
+    public T removeLast(){
+       if(size ==0){
+           return newList[0];
+       }
+       else {
+           T c;
+           c = newList[last - 1];
+           newList[last - 1] = null;
+           last -= 1;
+           size -= 1;
+           step -= 1;
+           return c;
+       }
     }
-    public Item removeFirst(){
+    public T removeFirst(){
         /*Item b [];
         b = (Item[]) new Object[8];
         System.arraycopy(newList,0,b,0,size-1);
          newList = b;*/
-        Item c;
-        c = newList[first+1];
-        newList[first + 1] = null;
-        first += 1;
-        size -= 1;
-        step -= 1;
-        return c;
-
+        if(size == 0 ){
+            return newList[0];
+        }
+        else {
+            T c;
+            c = newList[first + 1];
+            newList[first + 1] = null;
+            first += 1;
+            size -= 1;
+            step -= 1;
+            return c;
+        }
     }
-    public Item get(int x ){
-        Item c;
+    public T get(int x ){
+        T c;
         c = newList[x - 1];
         return c;
     }
@@ -90,11 +98,11 @@ public class ArrayDeque<Item> {
     }
     public boolean isEmpty(){
         if ( size == 0){
-            return true;
+            return false;
 
         }
         else {
-            return false;
+            return true;
         }
     }
 
