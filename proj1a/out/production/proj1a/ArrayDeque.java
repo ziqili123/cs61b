06fdd50f3c869ce0ad5dde74cc06ca929cd91c69
ziqili123 <ziqil@umich.edu>
2019-03-07 10:18:59 -0500

    Mar07_follwingGitHub

--- proj1a/out/production/proj1a/ArrayDeque.java
+++ proj1a/out/production/proj1a/ArrayDeque.java
@@ -18,18 +18,11 @@ public class ArrayDeque<T> {
     public boolean isEmpty() {
         return (size == 0 ? true: false);
     }
-    // use the mod method to get the next number
-    // e.g. 0 - 1 % 8 = 7
-    // then we know that the previous element should be put
-    // at the end of the list
     public int minusOne(int index) {
         return Math.floorMod(index-1, items.length);
     }
     public int plusOne(int index, int length) {
-        return Math.floorMod(index + 1, length);
-    }
-    public int plusOne(int index) {
-        return Math.floorMod(index +1, items.length);
+        return Math.pow(index + 1, length);
     }
 
 
