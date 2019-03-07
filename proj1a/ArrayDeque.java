8ec7dd908b7ae69b4247de1aca8f46ae0fda0ba3
ziqili123 <ziqil@umich.edu>
2019-03-07 14:15:27 -0500

    sytle check modification & API

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -13,23 +13,23 @@ public class ArrayDeque<T> {
 
     }
     public int size() {
-    return size;
+        return size;
     }
     public boolean isEmpty() {
-        return (size == 0 ? true: false);
+        return (size == 0 ? true : false);
     }
     // use the mod method to get the next number
     // e.g. 0 - 1 % 8 = 7
     // then we know that the previous element should be put
     // at the end of the list
     public int minusOne(int index) {
-        return Math.floorMod(index-1, items.length);
+        return Math.floorMod(index - 1, items.length);
     }
     public int plusOne(int index, int length) {
         return Math.floorMod(index + 1, length);
     }
     public int plusOne(int index) {
-        return Math.floorMod(index +1, items.length);
+        return Math.floorMod(index + 1, items.length);
     }
     /** resize
      *  if the memory is full, expand()
@@ -38,11 +38,11 @@ public class ArrayDeque<T> {
      *
      */
     private void resize() {
-        if(size == items.length) {
+        if (size == items.length) {
             expand();
         }
 
-        if(size < items.length / 4 && items.length > 8) {
+        if (size < items.length / 4 && items.length > 8) {
             reduce();
         }
 
