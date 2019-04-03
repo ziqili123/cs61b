f898b995f58d9c6f677d880a59a2031b0c288d4e
ziqili123 <ziqil@umich.edu>
2019-04-03 08:45:18 -0400

    Update Main.java

--- lab14/lab14/Main.java
+++ lab14/lab14/Main.java
@@ -1,7 +1,14 @@
 package lab14;
 
+import lab14lib.Generator;
+import lab14lib.GeneratorAudioVisualizer;
+import lab14lib.GeneratorDrawer;
+import lab14lib.GeneratorPlayer;
+
 public class Main {
 	public static void main(String[] args) {
 		/** Your code here. */
+		Generator g1 = new SineWaveGenerator(200);
+
 	}
 } 
